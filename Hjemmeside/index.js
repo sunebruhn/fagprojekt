13254d3368f6ba85aec69aa5592900f9f91b3281
EmiloK <79467203+EmiloK@users.noreply.github.com>
2021-06-07 14:45:39 +0200

    Hjemmeside update

--- Hjemmeside/index.js
+++ Hjemmeside/index.js
@@ -9,6 +9,20 @@ function ON()  {
   x.innerHTML = "The LED is currently ON";
 }
 
+function LEDswitch(button)  {
+  var x = document.getElementById(button);
+  switch (x.innerHTML)  {
+    case "On":
+      x.innerHTML = "Off";
+      document.getElementById(button).style.background = "Red";
+      break;
+    case "Off":
+      x.innerHTML = "On";
+      document.getElementById(button).style.background = "LawnGreen";
+      break;
+  }
+}
+
 function MatrixState(button) {
   var x = document.getElementById(button);
   switch (x.innerHTML)  {
