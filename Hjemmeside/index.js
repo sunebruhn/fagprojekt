d670c0027988dc2813383691f2dce802a6731643
EmiloK <Emil.bruunkrogh@gmail.com>
2021-06-08 11:18:21 +0200

    console output

--- Hjemmeside/index.js
+++ Hjemmeside/index.js
@@ -11,14 +11,18 @@ function ON()  {
 
 function LEDswitch(button)  {
   var x = document.getElementById(button);
+  var on = 1;
+  var off = 0;
   switch (x.innerHTML)  {
     case "On":
       x.innerHTML = "Off";
       document.getElementById(button).style.background = "Red";
+      console.log(off);
       break;
     case "Off":
       x.innerHTML = "On";
       document.getElementById(button).style.background = "LawnGreen";
+      console.log(on);
       break;
   }
 }
