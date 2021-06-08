fac84b6e9b0aa255ec79b4e9785dbc806cd6818b
EmiloK <Emil.bruunkrogh@gmail.com>
2021-06-08 21:05:26 +0200

    merge

--- Hjemmeside/index.js
+++ Hjemmeside/index.js
@@ -1,14 +1,3 @@
-
-function OFF()  {
-  var x = document.getElementById('LEDState');
-  x.innerHTML = "The LED is currently OFF";
-}
-
-function ON()  {
-  var x = document.getElementById('LEDState');
-  x.innerHTML = "The LED is currently ON";
-}
-
 function LEDswitch(button)  {
   var x = document.getElementById(button);
   var on = 1;
@@ -23,20 +12,6 @@ function LEDswitch(button)  {
       x.innerHTML = "On";
       document.getElementById(button).style.background = "LawnGreen";
       console.log(on);
-      break;
-  }
-}
-
-function MatrixState(button) {
-  var x = document.getElementById(button);
-  switch (x.innerHTML)  {
-    case " ":
-      x.innerHTML = "  ";
-      document.getElementById(button).style.background = "green";
-      break;
-    case "  ":
-      x.innerHTML = " ";
-      document.getElementById(button).style.background = "grey";
       break;
   }
 }
