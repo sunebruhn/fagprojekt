3ce1a63764b08e56ce35a5e2f21f544e86c852c5
EmiloK <79467203+EmiloK@users.noreply.github.com>
2021-06-11 11:27:51 +0200

    final merge

--- Kode/Smart Lighting/data/script.js
+++ Kode/Smart Lighting/data/script.js
@@ -1,5 +1,19 @@
-var ws = new WebSocket("ws://192.168.2.69/test");
+var ws = new WebSocket("ws://192.168.1.69/test");
 
-function buttonState()  {
-	ws.send("Button pressed");
+function LEDswitch(button)  {
+  var x = document.getElementById(button);
+  var on = 1;
+  var off = 0;
+  switch (x.innerHTML)  {
+    case "On":
+      x.innerHTML = "Off";
+      document.getElementById(button).style.background = "Red";
+      ws.send(JSON.stringify({'id': 2, 'state': 0}));
+      break;
+    case "Off":
+      x.innerHTML = "On";
+      document.getElementById(button).style.background = "LawnGreen";
+      ws.send(JSON.stringify({'id': 1, 'state': 1}));;
+      break;
+  }
 }
