a1a8f075e8aad00c63d7c6ac6f05c520dddd1f91
EmiloK <79467203+EmiloK@users.noreply.github.com>
2021-06-15 13:12:26 +0200

    bæ

--- Kode/Smart Lighting/data/script.js
+++ Kode/Smart Lighting/data/script.js
@@ -104,6 +104,7 @@ function getState() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       const jsonresponse = JSON.parse(this.responseText);
+      document.getElementById('LED_State').innerHTML = jsonresponse.state;
       LEDSTATE = jsonresponse.state;
     }
   };
